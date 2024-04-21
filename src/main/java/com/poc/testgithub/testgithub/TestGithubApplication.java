8cd3acaa3254b153ae73a40be25d59de0bf51803
AmirHussain <barshi631@gmail.com>
2024-04-21 19:47:09 +0530

    test

---
+++ src/main/java/com/poc/testgithub/testgithub/TestGithubApplication.java
@@ -0,0 +1,15 @@
+package com.poc.testgithub.testgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGithubApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TestGithubApplication.class, args);
+		System.out.println("Testing");
+	}
+
+}
